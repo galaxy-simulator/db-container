b423441909994423325eabeff118037d55a5cd77
Emile <hanemile@protonmail.com>
2019-02-09 19:02:59 +0100

    added a shebang and listing all the stars in go and csv format

--- insertdouble.sh
+++ insertdouble.sh
@@ -2,5 +2,8 @@
 curl -X POST http://localhost:8081/deleteStars
 curl -X POST http://localhost:8081/deleteNodes 
 curl -X POST --data "w=1000" http://localhost:8081/new
-curl -X POST --data "x=750&y=250&vx=0.1&vy=0.3&m=100&index=1" http://localhost:8081/insertStar
-curl -X POST --data "x=250&y=250&vx=0.1&vy=0.3&m=100&index=1" http://localhost:8081/insertStar
+curl -X POST --data "x=300&y=300&vx=0.1&vy=0.3&m=100&index=1" http://localhost:8081/insertStar
+curl -X POST --data "x=200&y=200&vx=0.1&vy=0.3&m=100&index=1" http://localhost:8081/insertStar
+curl -X POST --data "x=400&y=400&vx=0.1&vy=0.3&m=100&index=1" http://localhost:8081/insertStar
+curl -X GET http://localhost:8081/starlist/go
+curl -X GET http://localhost:8081/starlist/csv
